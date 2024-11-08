1272cecc227e4e7380660749b169e67634ac57f5
Luca Minasi <minasi.luca@iisgalvanimi.edu.it>
2024-11-08 10:35:07 +0100

    aggiunto programmino

---
+++ livr_programma.py
@@ -0,0 +1,9 @@
+somma= 0
+numero=int( input("INserici un numero intero (0 per terminare):"))
+
+while numero!=0:
+      somma+= numero
+      numero=int( input("INserici un numero intero (0 per terminare):"))
+      
+    
+print("La somma  di tutti i numeri insertiti è:", somma)
